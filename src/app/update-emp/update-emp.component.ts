5fa960ced0767062318384bbdd564c908782ad66
shantanu <sbtalk@gmail.com>
2019-10-21 16:57:10 +0530

    update-emp added

--- src/app/update-emp/update-emp.component.ts
+++ src/app/update-emp/update-emp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Emp } from '../emp';
+import { EmpService } from '../emp.service';
 
 @Component({
   selector: 'app-update-emp',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-emp.component.css']
 })
 export class UpdateEmpComponent implements OnInit {
-
-  constructor() { }
+  showFlag: boolean = false;
+  showForm:boolean=false;
+  public emp: Emp;
+  empId: number = 1;
+  constructor(private _service: EmpService) { }
 
   ngOnInit() {
   }
+  onSubmit() {
+    console.log("data submitted")
+    this._service.find(this.empId).subscribe(data => this.emp = data, error => console.log(error), () => this.toggle())
+  }
+  toggle(): void {
+    this.showFlag = true;
+  }
 
+  doEdit():void{
+    this.showFlag = false;
+    this.showForm=true;
+  }
+
+  onSubmit2() {
+    console.log("data submitted")
+    this.showFlag = false;
+    this.showForm=false;
+  }
 }
